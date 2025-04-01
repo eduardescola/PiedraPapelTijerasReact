20e42e721e9bd39c1c4e5ca46ec77b885e1fbf52
eduardescola <edduardescola@gmail.com>
2025-04-01 16:42:39 +0200

    icono switch tema

--- src/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Switch } from '@mui/material';
+import { Switch, FormControlLabel } from '@mui/material';
+import { Brightness7, Brightness4 } from '@mui/icons-material';  // Importar los íconos de sol y luna
 import './ThemeSwitchStyles.css';
 
 const ThemeSwitch: React.FC = () => {
@@ -19,10 +20,30 @@ const ThemeSwitch: React.FC = () => {
   };
 
   return (
-    <Switch
-      checked={darkMode}
-      onChange={handleThemeChange}
-      inputProps={{ 'aria-label': 'Switch theme' }}
+    <FormControlLabel
+      control={
+        <Switch
+          checked={darkMode}
+          onChange={handleThemeChange}
+          inputProps={{ 'aria-label': 'Switch theme' }}
+          color="default"
+          sx={{ 
+            '& .MuiSwitch-thumb': {
+              backgroundColor: darkMode ? '#ffcc00' : '#f57c00',
+            },
+            '& .MuiSwitch-track': {
+              backgroundColor: darkMode ? '#757575' : '#e0e0e0',
+            },
+          }}
+        />
+      }
+      labelPlacement="start"  // Coloca los íconos a la izquierda
+      label={
+        <>
+          <Brightness7 sx={{ display: !darkMode ? 'block' : 'none', color: '#FFB300', fontSize: 24 }} />  {/* Sol en modo claro */}
+          <Brightness4 sx={{ display: darkMode ? 'block' : 'none', color: '#fff', fontSize: 24 }} />  {/* Luna en modo oscuro */}
+        </>
+      }
     />
   );
 };
